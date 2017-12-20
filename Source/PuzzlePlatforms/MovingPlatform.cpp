3d3008d7967f4e48a68ce10019bb43fb8de998a8
Liam Daniel Hurt <liamhurt51@gmail.com>
2017-12-20 15:09:29 -0800

    Commited [master 1ba7bec] Added the ability for the platform to return to its original position after reaching the target destination..

--- Source/PuzzlePlatforms/MovingPlatform.cpp
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -2,6 +2,40 @@
 
 #include "MovingPlatform.h"
 
+AMovingPlatform::AMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
 
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority()) {
+		SetReplicates(true);
+		SetReplicateMovement(true);
+	}
+
+	GlobalStartLocation = GetActorLocation();
+	GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
+}
+
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (HasAuthority()) {
+		FVector Location = GetActorLocation();
+
+		if ((Location - GlobalStartLocation).Size() >= (GlobalTargetLocation - GlobalStartLocation).Size()) {
+			FVector Tmp = GlobalTargetLocation;
+			GlobalTargetLocation = GlobalStartLocation;
+			GlobalStartLocation = Tmp;
+		}
+		SetActorLocation(Location += Speed * DeltaTime * (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal());
+	}
+}
 
 
--- Source/PuzzlePlatforms/MovingPlatform.h
+++ Source/PuzzlePlatforms/MovingPlatform.h
@@ -14,7 +14,24 @@ class PUZZLEPLATFORMS_API AMovingPlatform : public AStaticMeshActor
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings")
+		float Speed = 20.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings", Meta = (MakeEditWidget = true))
+		FVector TargetLocation;
+
+	AMovingPlatform();
+
+	virtual void BeginPlay() override;
 	
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	// Actors start location in global space
+	FVector GlobalStartLocation;
 	
-	
+	// Actors target location in global space
+	FVector GlobalTargetLocation;
+
 };
