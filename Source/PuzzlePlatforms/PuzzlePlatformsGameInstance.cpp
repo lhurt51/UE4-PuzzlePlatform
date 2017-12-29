502f4f5d61e06c4504071197c66038bd2ed4e7d0
Liam Daniel Hurt <liamhurt51@gmail.com>
2017-12-28 18:48:48 -0800

    Learning about the game instance and why it is useful to the game developer

---
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PuzzlePlatformsGameInstance.h"
+
+UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer &ObjectInitializer)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Constructor()"));
+}
+
+void UPuzzlePlatformsGameInstance::Init()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Init()"));
+}
